c9f928488cf6544d1275f7a6872f9a1b50a7f52f
Trey Rader <treyrdr09@gmail.com>
2016-09-02 14:31:54 -0500

    test

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 $(function() {
-	$('#search-term').submit(function (event) {
+	$('#search-term').submit(function (event) { //the <button> has to have a type="submit"
 		event.preventDefault(); //need this to prevent the page from refreshing since that is the default functionality
 
 		var searchTerm = $('#query').val();
@@ -20,21 +20,25 @@ $(function() {
     	var params = {
       		
       		apikey: "876a23160e89575e71ba5d7851842cb6", //look at the output format on the API doc
-      		//q: searchTerm,
+      		q: searchTerm,
       		format: 'JSONP',
-      		q_track: searchTerm,
+      		// callback parameter is required by the API. However 
+       		// jQuery automatically adds this with the "jsonp: 'callback'" property passed to the ajax call
+       		// callback: callbackFunction 
  		}
     
     	$.ajax({
 			type: 'GET',
 			url: "http://api.musixmatch.com/ws/1.1/track.search",
 			data: params,
+			dataType: 'jsonp',// tells jQuery to make a jsonp request
+			jsonp: 'callback', // jQuery automatically adds callback=someRandomFunctionName
+			// to the API request GET parameters (params)
 			success: function(data) {
-				console.log(data.message);
-				//showResults(data.track);
-				$('#search-results').html("<li>" +
-					data.message +
-					"</li>");
+				//console.log(data.track_list);
+				showResults(data);
+				//$('#search-results').html("<li>" +
+					
 			},
 			error: function() {
 				$('#feed').text("couldn't retrieve data");
@@ -43,20 +47,21 @@ $(function() {
 	}
 
 function showResults(results) {
-	var html = "test";
+	var html = "";
 
   $.each(results, function(index, value){
-  	console.log(value)
+  		var trackList = value.body.track_list;
+  		console.log(trackList)
+  		html += '<li>' +trackList[0].track.album_name+ '</li><br/>';
+  	}); //the 'target' attribute tells the browser to open the linked url in a different window.
+
+  $('#search-results').html(html);
  
-  }); 
+  
   //$('#search-results').html(html);
 
 }
 
 	
-
-
-
-
 
 
