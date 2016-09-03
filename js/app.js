35fbf09b851a8121850d29b61c075b472a8bc36c
Trey Rader <treyrdr09@gmail.com>
2016-09-03 15:28:21 -0500

    new containers and ideas for layouts

--- js/app.js
+++ js/app.js
@@ -4,10 +4,12 @@ $(function() {
 	$('#search-term').submit(function (event) { //the <button> has to have a type="submit"
 		event.preventDefault(); //need this to prevent the page from refreshing since that is the default functionality
 
-		var searchTerm = $('#query').val();
+		//var searchTerm = $('#query').val();
     $('#query').val(function() {
       $(this).focus('');
     })
+
+    	var searchTerm = $('#query').val();
     
     
 		getRequest(searchTerm);
@@ -29,16 +31,18 @@ $(function() {
     
     	$.ajax({
 			type: 'GET',
-			url: "https://api.musixmatch.com/ws/1.1/track.search",
+			url: "https://api.musixmatch.com/ws/1.1/track.lryics.get",
 			data: params,
 			dataType: 'jsonp',// tells jQuery to make a jsonp request
 			jsonp: 'callback', // jQuery automatically adds callback=someRandomFunctionName
 			// to the API request GET parameters (params)
 			success: function(data) {
-				//console.log(data.track_list);
-				showResults(data);
-				//$('#search-results').html("<li>" +
-					
+
+				console.log(data);
+				var trackLyrics = data.message.header;
+				//var track = trackList.track;
+				//showLyrics(trackLyrics);
+				console.log(trackLyrics);
 			},
 			error: function() {
 				$('#feed').text("couldn't retrieve data");
@@ -46,21 +50,20 @@ $(function() {
 		});
 	}
 
-function showResults(results) {
+function showLyrics(trackLyrics) {
 	var html = "";
 
-  $.each(results, function(index, value){
-  		var trackList = value.body.track_list;
-  		console.log(trackList)
-  		html += '<li>' +trackList[0].track.album_name+ '</li><br/>';
+  $.each(trackLyrics, function(index, lyrics){
+  		console.log("looping... " + lyrics);
+  		//var trackName = track.track.track_name;
+  		var songLyrics = lyrics.lyrics_body;
+  		html += '<li>' +songLyrics+ '</li><br/>';
   	}); //the 'target' attribute tells the browser to open the linked url in a different window.
 
   $('#search-results').html(html);
  
-  
-  //$('#search-results').html(html);
-
 }
+
 
 	
 
