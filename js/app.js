4d412a397c8e5de3b0b5471f9d5ed671ec05d43a
Trey Rader <treyrdr09@gmail.com>
2016-09-03 17:56:45 -0500

    youtube api connected

--- js/app.js
+++ js/app.js
@@ -65,6 +65,64 @@ function showLyrics(trackLyrics) {
 }
 
 
+//you tube info below
+
+
+$(function() {
+	$('#search-term').submit(function (event) {
+		event.preventDefault(); //need this to prevent the page from refreshing since that is the default functionality
+
+		var searchTerm = $('#query').val();
+    $('#query').val(function() {
+      $(this).focus('');
+    })
+    
+    
+		getVideos(searchTerm);
+	})
+})
+
+
+
+function getVideos(searchTerm) {
+    // query parameters the youtube api supports
+    var params = {
+      part: 'snippet',
+      // my api developer key has to be included
+      key: 'AIzaSyD52Xj44R7VjWXQZxGm-x3t2DjzZXfi288',
+      //
+      q: searchTerm
+    }
+    // the url (endpoint). AN endpoint is just a special kind of url
+    var url ='https://www.googleapis.com/youtube/v3/search'
+
+    
+    $.getJSON(url, params, function(data) {
+      console.log(data);
+      showResults(data.items);
+      console.log("the data items are " + data.items);
+    })
+  }
+
+
+
+function showResults(videos) {
+	var html = "";
+
+  $.each(videos, function(index, video){
+//must include 'value' inside variables, becaue 'value', in this context, is equal to 'results', which = 'data.items'
+
+    var thumbnail = video.snippet.thumbnails.medium.url; //same as saying data.items.snippet....
+    var title = video.snippet.title; 
+    var videoURL = "https://www.youtube.com/watch?v=" + video.id.videoId;
+
+  	html += '<li><a href="' +videoURL+ '" target="_blank"><h3>' +title+ '</h3></a><br/><img src="' +thumbnail+ ' /></li><br/>';
+  }); //the 'target' attribute tells the browser to open the linked url in a different window.
+
+  $('#videos-list').html(html);
+
+}
+
 	
 
 
