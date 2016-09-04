f3a6cfc2720aa86fe94d24d78cfb8c97186fdf30
Trey Rader <treyrdr09@gmail.com>
2016-09-04 10:59:13 -0500

    two apis are semi-working

--- js/app.js
+++ js/app.js
@@ -3,16 +3,9 @@
 $(function() {
 	$('#search-term').submit(function (event) { //the <button> has to have a type="submit"
 		event.preventDefault(); //need this to prevent the page from refreshing since that is the default functionality
-
-		//var searchTerm = $('#query').val();
-    $('#query').val(function() {
-      $(this).focus('');
-    })
-
-    	var searchTerm = $('#query').val();
-    
-    
-		getRequest(searchTerm);
+    	var searchTerm = $('#query').val(); //define variable last
+    	getRequest(searchTerm);
+    	getVideos(searchTerm);
 	})
 })
 	
@@ -24,14 +17,11 @@ $(function() {
       		apikey: "876a23160e89575e71ba5d7851842cb6", //look at the output format on the API doc
       		q: searchTerm,
       		format: 'JSONP',
-      		// callback parameter is required by the API. However 
-       		// jQuery automatically adds this with the "jsonp: 'callback'" property passed to the ajax call
-       		// callback: callbackFunction 
- 		}
+		}
     
     	$.ajax({
 			type: 'GET',
-			url: "https://api.musixmatch.com/ws/1.1/track.lryics.get",
+			url: "https://api.musixmatch.com/ws/1.1/track.search",
 			data: params,
 			dataType: 'jsonp',// tells jQuery to make a jsonp request
 			jsonp: 'callback', // jQuery automatically adds callback=someRandomFunctionName
@@ -39,10 +29,10 @@ $(function() {
 			success: function(data) {
 
 				console.log(data);
-				var trackLyrics = data.message.header;
+				var trackList = data.message.body.track_list;
 				//var track = trackList.track;
-				showLyrics(trackLyrics);
-				console.log("the track info " +trackLyrics);
+				showTracks(trackList);
+				console.log("the tracklists are: " +trackList);
 			},
 			error: function() {
 				$('#lyrics-list').text("couldn't retrieve data");
@@ -50,15 +40,14 @@ $(function() {
 		});
 	}
 
-function showLyrics(trackLyrics) {
+function showTracks(trackList) {
 	var html = "";
 
-  $.each(trackLyrics, function(index, lyrics){
-  		console.log("looping... " + lyrics);
-  		//var trackName = track.track.track_name;
-  		var songLyrics = lyrics.lyrics_body;
-  		html += '<li>' +songLyrics+ '</li><br/>';
-  	}); //the 'target' attribute tells the browser to open the linked url in a different window.
+  $.each(trackList, function(index, tracks){
+  		console.log("looping thru trackList");
+  		var artist = tracks.track.artist_name;
+  		html += '<li>' +artist+ '</li><br/>';
+  	}); 
 
   $('#lyrics-list').html(html);
  
@@ -74,38 +63,22 @@ function showLyrics(trackLyrics) {
 //you tube info below
 
 
-$(function() {
-	$('#search-term').submit(function (event) {
-		event.preventDefault(); //need this to prevent the page from refreshing since that is the default functionality
-
-		var searchTerm = $('#query').val();
-    $('#query').val(function() {
-      $(this).focus('');
-    })
-    
-    
-		getVideos(searchTerm);
-	})
-})
-
-
 
 function getVideos(searchTerm) {
     // query parameters the youtube api supports
     var params = {
       part: 'snippet',
-      // my api developer key has to be included
       key: 'AIzaSyD52Xj44R7VjWXQZxGm-x3t2DjzZXfi288',
-      //
       q: searchTerm
     }
-    // the url (endpoint). AN endpoint is just a special kind of url
     var url ='https://www.googleapis.com/youtube/v3/search';
 
     
     $.getJSON(url, params, function(data) {
       console.log(data);
       showResults(data.items);
+      $('#query').val('');
+      
     })
   }
 
@@ -115,13 +88,13 @@ function showResults(videos) {
 	var html = "";
 
   $.each(videos, function(index, video){
-//must include 'value' inside variables, becaue 'value', in this context, is equal to 'results', which = 'data.items'
 
-    var thumbnail = video.snippet.thumbnails.default.url; //same as saying data.items.snippet....
-    var title = video.snippet.title; 
-    var videoURL = "https://www.youtube.com/watch?v=" + video.id.videoId;
+    var thumbnail = video.snippet.thumbnails.default.url; 
+    var title = video.snippet.title;
+    var videoId = video.id.videoId; 
+    var videoURL = "https://www.youtube.com/watch?v=" + videoId;
 
-  	html += '<li><a href="' +videoURL+ '" target="_blank"><h5>' +title+ '</h5></a><br/><img src="' +thumbnail+ ' /></li><br/>';
+  	html += '<li><h5>' +title+ '</h5><br/><a href="' +videoURL+ '" target="_blank"><img src="' +thumbnail+ ' /></a></li><br/>';
   }); //the 'target' attribute tells the browser to open the linked url in a different window.
 
   $('#videos-list').html(html);
@@ -130,4 +103,28 @@ function showResults(videos) {
 
 	
 
+	//mediawiki api below
 
+	/*function getArtistInfo(searchTerm) {
+		var params = {
+			url: "https://en.wikipedia.org/w/api.php",
+			format: "JSONP",
+			q: searchTerm,
+			action: "query",
+			maxlag: "",
+			prop: "info"
+		}
+
+		$.ajax({
+			type: 'GET',
+			//url: "https://en.wikipedia.org/w/api.php",
+			data: params,
+			dataType: 'jsonp',
+			jsonp: 'callback',
+			success: function(data) {
+				console.log("wiki data is: " +data);
+			}
+		})
+	}
+
+*/
