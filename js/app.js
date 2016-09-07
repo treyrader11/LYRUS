ed13ace21f10f5c7aba1ca0f5f49ad30d6a40772
Trey Rader <treyrdr09@gmail.com>
2016-09-07 12:21:07 -0500

    vid is up in modal/new js file deleted

--- js/app.js
+++ js/app.js
@@ -1,45 +1,50 @@
 'use strict'
 
+
 $(function() {
-	$('#search-term').submit(function (event) { //the <button> has to have a type="submit"
-		event.preventDefault(); //need this to prevent the page from refreshing since that is the default functionality
+
+	var $searchForm = $('#search-term');
+	
+	$searchForm.submit(function(e) { //the <button> has to have a type="submit"
+		e.preventDefault(); //need this to prevent the page from refreshing since that is the default functionality
     	var searchTerm = $('#query').val(); //define variable last
-    	getTracks(searchTerm);
+    	getTrackNames(searchTerm);
     	getVideos(searchTerm);
-	})
+	});
 })
-	
-	
-	function getTracks(searchTerm) {
-    
-    	var params = {
-      		
-      		apikey: "876a23160e89575e71ba5d7851842cb6", //look at the output format on the API doc
-      		q: searchTerm,
-      		format: 'JSONP',
-		}
-    
-    	$.ajax({
-			type: 'GET',
-			url: "https://api.musixmatch.com/ws/1.1/track.search",
-			data: params,
-			dataType: 'jsonp',// tells jQuery to make a jsonp request
-			jsonp: 'callback', // jQuery automatically adds callback=someRandomFunctionName
-			// to the API request GET parameters (params)
-			success: function(data) {
 
-				
-				var trackList = data.message.body.track_list;
-				//console.log(data);
-				showTracks(trackList);
-			},
-			error: function() {
-				$('#lyrics-list').text("couldn't retrieve data");
-			}
-		});
+
+//makes an AJAX call to the restful musiXmatch api to retreive data
+
+function getTrackNames(searchTerm) {
+    
+    var params = {
+      	apikey: "876a23160e89575e71ba5d7851842cb6",
+      	q: searchTerm,
+      	format: 'JSONP',
 	}
+    
+    $.ajax({
+		type: 'GET',
+		url: "https://api.musixmatch.com/ws/1.1/track.search",
+		data: params,
+		dataType: 'jsonp',
+		jsonp: 'callback', // jQuery automatically adds callback=someRandomFunctionName
+		// to the API request GET parameters (params)
+		success: function(data) {
+			var trackList = data.message.body.track_list;
+			//console.log(trackList);
+			showTrackNames(trackList);
+		},
+		error: function() {
+			$('#lyrics-list').text("Sorry, but we're having difficulties retrieving the data");
+		}
+	});
+}
 
-function showTracks(trackList) {
+//a function to loop thru themusiXmatch track data
+
+function showTrackNames(trackList) {
 	var html = "";
 
   $.each(trackList, function(index, tracks){
@@ -50,72 +55,55 @@ function showTracks(trackList) {
   		html += '<li><a href="#lyrics-modal" data-toggle="modal" data-track_id="' +trackId+ '">' +artist+ ': ' +trackName+ '</a></li><br/>';
   	}); 
 
-  $('#lyrics-list').html(html);
-
- 
+  $('ul#lyrics-list').html(html); //this html is a <li> in which a link targets a modal window
 }
 
-
-function getLyrics(track_id) {
-    
-    	var params = {
-      		
-      		apikey: "876a23160e89575e71ba5d7851842cb6", //look at the output format on the API doc
-      		format: 'JSONP',
-      		track_id: track_id,
-		}
-    
-    	$.ajax({
-			type: 'GET',
-			url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get",
-			data: params,
-			dataType: 'jsonp',// tells jQuery to make a jsonp request
-			jsonp: 'callback', // jQuery automatically adds callback=someRandomFunctionName
-			// to the API request GET parameters (params)
-			success: function(data) {
-
-				
-				var lyrics = data.message.body.lyrics.lyrics_body;
-				//console.log(data);
-				showLyrics(lyrics);
-			},
-			error: function() {
-				$('#lyrics-list').text("couldn't retrieve data");
-			}
-		});
-	}
-
-function showLyrics(trackLyrics) {
-	var html = trackLyrics;
-
-
-
-  $('#lyrics').html(html);
-
- 
-}
-
-
-$('#lyrics-list').on('click', 'a', function(e) {
+$('ul#lyrics-list').on('click', 'a', function(e) {
     e.preventDefault();
 
-    var track_id = $(this).data('track_id');
+    var track_id = $(this).data('track_id'); //this is how you transfer data 
+    //that are contained within a variable of another function.
+    //We're getting the value of 'data-track_id' from showTrackNames() and
+    //passing it to getLyrics().
+    
     getLyrics(track_id);
 
 })
 
+function getLyrics(track_id) {
+    //We need to make a new ajax call because there's a 
+    //different endpoint for obtaining the lyrics to tracks
+    var params = {
+      	apikey: "876a23160e89575e71ba5d7851842cb6", //look at the output format on the API doc
+      	format: 'JSONP',
+      	track_id: track_id,
+		}
+    
+    $.ajax({
+		type: 'GET',
+		url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get",
+		data: params,
+		dataType: 'jsonp',// tells jQuery to make a jsonp request
+		jsonp: 'callback', // jQuery automatically adds callback=someRandomFunctionName
+			// to the API request GET parameters (params)
+		success: function(data) {
+			var lyrics = data.message.body.lyrics.lyrics_body;
+			//console.log(data);
+			showLyrics(lyrics);
+		},
+	});
+}
 
+function showLyrics(lyrics) {
+	var html = lyrics;
 
+	$('ul#lyrics').html(html);
+}
 
-
-
-
-//you tube info below
 
 
 
 function getVideos(searchTerm) {
-    // query parameters the youtube api supports
     var params = {
       part: 'snippet',
       key: 'AIzaSyD52Xj44R7VjWXQZxGm-x3t2DjzZXfi288',
@@ -124,48 +112,81 @@ function getVideos(searchTerm) {
     var url ='https://www.googleapis.com/youtube/v3/search';
 
     
-    $.getJSON(url, params, function(data) { //another way to make a ajax GETrequest with jQuery
-      //console.log(data);
-      showVideos(data.items);
-      $('#query').val('');
-      
-    })
+    $.ajax({
+    	type: 'GET',
+    	url: "https://www.googleapis.com/youtube/v3/search",
+    	data: params,
+    	dataType: 'jsonp',
+    	success: function(data) {
+    		console.log(data.items);
+    		var itemsArr = data.items;
+    		showVideos(itemsArr);
+    		$('#query').val();
+    	},
+    	error: function() {
+    		$('#videos-list').text("Sorry, but we're having difficulties with retrieving the data");
+    	}
+    });
+
   }
 
-
-
-function showVideos(videos) {
+  function showVideos(itemsArr) {
 	var html = "";
 
-  $.each(videos, function(index, video){
+  	$.each(itemsArr, function(index, item){
 
-    var thumbnail = video.snippet.thumbnails.default.url; 
-    var title = video.snippet.title;
-    var videoId = video.id.videoId; 
-    var videoURL = "https://www.youtube.com/watch?v=" + videoId;
-    var lightBox = '<iframe width="300" height="230" src="https://www.youtube.com/embed/' +videoId+ '" frameborder="0" allowfullscreen></iframe>';
+    	var thumbnail = item.snippet.thumbnails.default.url; 
+    	var title = item.snippet.title;
+    	var videoId = item.id.videoId; 
+    	var videoURL = "https://www.youtube.com/watch?v=" + videoId;
+   		// var lightBox = '<iframe width="300" height="230" src="https://www.youtube.com/embed/' +videoId+ '" frameborder="0" allowfullscreen></iframe>';
+   		var embedURL = "https://www.youtube.com/embed/" +videoId;
 
-  	html += '<li><h5>' +title+ '</h5><br/><a href="#video-modal" data-toggle="modal"><img src="' +thumbnail+ '"/></a></li><br/>';
+  		html += '<li><h5>' +title+ '</h5><br/><a href="#video-modal" data-toggle="modal" data-embed="' +embedURL+ '"><img src="' +thumbnail+ '"/></a></li><br/>';
   
-
-		$('#videos-list').on('click', 'a', function(e) {
-    		e.preventDefault();
-
-    		$('#videos').html('<li>' +lightBox+ '</li>');
-    	})
 	});
 
   	$('#videos-list').html(html)
 
 }
-   //the 'target' attribute tells the browser to open the linked url in a different window.
+
+$('#videos-list').on('click', 'a', function(e) {
+    e.preventDefault();
+
+    var URL = $(this).data('embed'); //getting the data from 'embedVideo'
+    //that is contained within the 'a' (link).
+    showLightbox(URL);
+    //now passing the data to the showLightbox function.
+ })
+
+function showLightbox(URL) {
+	var html = '<li><iframe width="300" height="230" src="' +URL+ '" frameborder="0" allowfullscreen></iframe></li>';
+	$('ul#video').html(html);
+	//giving the modal-box <ul> all of this new html that has some store data from our ajax call
+}
 
 
 
 
 
 
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
