62ea49d856e301b6406c612423c5ec70589c75a5
Trey Rader <treyrdr09@gmail.com>
2016-09-08 20:56:35 -0500

    track name added in lyrics modal

--- js/apis.js
+++ js/apis.js
@@ -18,9 +18,11 @@ function getTracks(track_id) {
 		jsonp: 'callback',
 		success: function(data) {
 			var trackURL = data.message.body.track.track_share_url;
-			console.log(trackURL);
-			//showTrackNames(trackList);
+			var trackName = data.message.body.track.track_name;
+			console.log('the name of the track is ' +trackName); 
 			var lyricsLink = '<br/><a href="' +trackURL+ '" target="_blank"><li id="full-lyrics" class="pull-left link">See full lyrics here</li></a>'; 
+			var track_name = '<h4>Lyrics for "' +trackName+ '"</h4>';
+			$('#lyrics-modal .modal-header').html(track_name);
 			$('#lyrics-footer').prepend(lyricsLink);
 			//added .link so that when btn is clicked, it'll remove the previous link since
 			//the the markup is being appended.
